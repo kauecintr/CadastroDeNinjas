a6f6ff55a7ec01674a8303aee5d86ca4e8ca189b
kauecintr <kaumon98@gmail.com>
2025-03-19 16:48:15 -0300

    first commit

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjaApplication.java
@@ -0,0 +1,13 @@
+package dev.java10x.CadastroDeNinjas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeNinjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeNinjaApplication.class, args);
+	}
+
+}
